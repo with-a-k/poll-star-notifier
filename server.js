1572ace06b65f9b4c38be5e50e7c54831bed443f
Erik Butcher <with_a_k@mac.com>
2015-11-16 17:53:46 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+  socket.on('message', function (channel, message) {
+    io.sockets.emit(channel + ':', message);
+  });
+});
+
+http.listen(process.env.PORT || 4000, function(){
+  console.log('Your server is up and running on Port 4000. Good job!');
+});
